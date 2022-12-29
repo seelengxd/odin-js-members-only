322ccfdfa7521f8243589d7c8bed04651180963f
see leng <seelengxd@gmail.com>
2022-12-29 08:48:56 +0800

    feat: add dummy user routes

---
+++ controllers/UserController.js
@@ -0,0 +1,11 @@
+exports.signUp = function (req, res, next) {
+  res.send("Sign up, not implemented");
+};
+
+exports.logIn = function (req, res, next) {
+  res.send("Log in, not implemented");
+};
+
+exports.logOut = function (req, res, next) {
+  res.send("Log out, not implemented");
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const UserController = require("../controllers/UserController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/sign-up", UserController.signUp);
+
+router.get("/log-in", UserController.logIn);
+
+router.get("/log-out", UserController.logOut);
 
 module.exports = router;
